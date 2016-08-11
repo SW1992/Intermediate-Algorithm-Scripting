dd05560c4303f6a776080e94f68c52fc5e2bee86
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-08-11 22:32:18 +0100

    Add files via upload

---
+++ Smallest-common-multiple.js
@@ -0,0 +1,39 @@
+// Smallest Common Multiple Bonfire Solution 
+function smallestCommons(arr) {
+  var numRange = [];
+  var x = 0;
+  var commonCount = 0;
+  var commonMulti = false;
+  var lowestCommonMulti;
+  arr.sort(function compareNum(a,b) {
+    // sort nums in descending order
+    return b - a;
+  }); 
+  for (var w = arr[0]; w >= arr[1]; w--) {
+    // loop down num range largest to smallest 
+    numRange.push(w);
+    // push num in range to array
+  } 
+  while (commonMulti === false) {
+    if (commonCount === numRange.length) {
+      commonMulti = true;
+      // stop loop
+    }
+    else {
+      commonCount = 0;
+      // reset counter 
+      x += numRange[0];
+      // increment x in multis of largest num, up to arbitrary val
+    }
+    for (var y = 0; y < numRange.length; y++) {
+      if ( x % numRange[y] === 0) {
+      // if x equally divisble into num in range
+        lowestCommonMulti = x;
+        commonCount++;
+      }
+    } 
+  }
+  return lowestCommonMulti;
+  // return result
+}
+smallestCommons([1,5]);
